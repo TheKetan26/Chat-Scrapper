ddef8364cc4c130b383220c7b99796fbfcf5d3ef
Ketang Solanki <ketangsolanki129@gmail.com>
2023-03-24 16:39:55 +0530

    Updated to classe.s

--- fileManager.py
+++ fileManager.py
@@ -1,4 +1,50 @@
-# class File:
-#     def __init__(self, file_exist, file_location):
-#         if file_exist:
-#             data =
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from os import getcwd
+from json import dumps
+
+
+class FileFolder:
+    def __init__(self, user, target):
+        self.user = user
+        self.target = target
+        self.driver = webdriver.Firefox()
+
+    def create_json(self):
+        print('Creating json file!')
+        words = {}
+        j = 1
+        while 1:
+            try:
+                location = f'{getcwd()}\{self.user}\messages\inbox\{self.target}\message_{j}.html'
+                self.driver.get(location)
+                i = 3
+                while 1:
+                    print(i, end='')
+                    try:
+                        name = (self.driver.find_element(By.XPATH, f'/html/body/div/div/div/div[2]/div[2]/div[{i}]/div[1]')).text
+                        line = (self.driver.find_element(By.XPATH, f'/html/body/div/div/div/div[2]/div[2]/div[{i}]/div[2]/div/div[2]')).text
+                        for word in line.split():
+                            if name in words:
+                                if word.lower() in words[name]:
+                                    words[name][word.lower()] += 1
+                                else:
+                                    words[name][word.lower()] = 1
+                            else:
+                                words[name] = {}
+                                words[name][word.lower()] = 1
+                    except NoSuchElementException:
+                        print('\nPage', j, 'complete')
+                        break
+                    print(''.join(['\b'] * len(str(i))), end='')
+                    i += 1
+                j += 1
+            except:
+                break
+        file = open('data.json', 'w')
+        file.write(dumps(words))
+        self.driver.close()
+
+    def print_json(self):
+        print(self.data)
--- main.py
+++ main.py
@@ -1,48 +1,29 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from os import getcwd
-from json import dumps
+from os.path import isfile, isdir
+
+from fileManager import FileFolder
 
 
-driver = webdriver.Firefox()
-driver.set_window_position(-10000, -10000)
+status = isfile('data.json')
 
-user = input("Enter folder name: ")
-target = input("Enter complete user name: ")
+if __name__ == '__main__':
+    if status:
+        print('File exists!')
+        pass
 
-words = {}
-print('Starting scrapping!')
-j = 1
-while 1:
-    try:
-        driver.get(f'{getcwd()}\{user}\messages\inbox\{target}\message_{j}.html')
-        i = 3
-        while 1:
-            print(i, end='')
-            try:
-                name = (driver.find_element(By.XPATH, f'/html/body/div/div/div/div[2]/div[2]/div[{i}]/div[1]')).text
-                line = (driver.find_element(By.XPATH, f'/html/body/div/div/div/div[2]/div[2]/div[{i}]/div[2]/div/div[2]')).text
-                for word in line.split():
-                    if name in words:
-                        if word.lower() in words[name]:
-                            words[name][word.lower()] += 1
-                        else:
-                            words[name][word.lower()] = 1
-                    else:
-                        words[name] = {}
-                        words[name][word.lower()] = 1
-            except NoSuchElementException:
-                print('\nPage', j, 'complete')
-                break
-            print(''.join(['\b'] * len(str(i))), end='')
-            i += 1
-        j += 1
-    except:
-        break
-print('Srapping ended!')
-
-file = open('data.json', 'w')
-file.write(dumps(words))
-
-driver.close()
+    else:
+        print("Json file not found!\nEnter following details to create new.")
+        user = input("Enter complete username: ").lower()
+        while not isdir(user):
+            print('Folder not found!')
+            user = input("Enter complete username: ").lower()
+        target = input("Enter complete target username: ").lower()
+        while not isdir(f'{user}\messages\inbox\{target}'):
+            print('Folder not found!')
+            target = input("Enter complete username: ").lower()
+        file = FileFolder(user, target)
+        try:
+            file.create_json()
+        except:
+            print('An unknown error occured!')
+        else:
+            print("File created! Start the app again to continue.")
